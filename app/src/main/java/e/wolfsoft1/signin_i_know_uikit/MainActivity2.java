e7ae432dd97ab5ef4c28bf22e8e4a0c6664f7eec
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-01 10:44:46 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/signin_i_know_uikit/MainActivity2.java
@@ -0,0 +1,19 @@
+package e.wolfsoft1.signin_i_know_uikit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+
+
+    }
+}
